9118398bc008d7e49276b2f457eeecebdbea41e5
Yusuf Semo <yas3176@mavs.uta.edu>
2023-10-21 22:20:16 -0500

    learn props

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ function App() {
   console.log('we are about to list the employees');
   const showEmployees = true;
   return (
+    
     <div className="App">
+      
       {showEmployees ? 
       <div>
-        <Employee></Employee>
-        <Employee></Employee>
-        <Employee></Employee>
+        <Employee name = "Yusuf" age={20} role="Intern"/>
+        <Employee name="abby" age={5}/>
+        <Employee name = "john"/>
+        <Employee/>
       </div>
       :<p>you cant see the employees</p> }
     </div>
+    
   );
 }
 
--- src/components/employee.js
+++ src/components/employee.js
@@ -1,6 +1,23 @@
-function Employee(){
-    return <h3>Here is an employee</h3>
+import PropTypes from 'prop-types'
+function Employee(props){
+    return(
+        <div className = "employes">
+     <h3>Employee {props.name}</h3>
+     <p>Age: {props.age}</p>
+     <p>{props.role}</p>
+     </div>
+    );
 
+}
+Employee.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    role: PropTypes.string,
+}
+Employee.defaultProps = {
+    name: "Guest",
+    age: 18,
+    role: "none",
 }
 
 export default Employee;
